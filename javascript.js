d4eca8dc833afdf39b4d0340b52956f6b0f5f650
David Lin <lindavid1998@gmail.com>
2022-08-17 07:59:16 -0700

    Add pseudocode to javascript.js

---
+++ javascript.js
@@ -0,0 +1,16 @@
+
+function playRockPaperScissors() {
+
+// While user wants to play
+
+    // Ask computer to choose rock, paper, or scissors and store in variable compAns
+
+    // Ask user to choose rock, paper, or scissors and store in variable userAns
+
+    // Show winner 
+
+    // Show score
+
+    // Ask user if they want to continue playing
+
+}
