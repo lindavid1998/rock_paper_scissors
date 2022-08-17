51c440fa84164849caf33bc3ed3e5e3d88ab44df
David Lin <lindavid1998@gmail.com>
2022-08-17 08:10:43 -0700

    Implement main function playRockPaperScissors

--- javascript.js
+++ javascript.js
@@ -1,16 +1,29 @@
-
 function playRockPaperScissors() {
 
-// While user wants to play
+    // initialize score
+    scores = {
+        computer: 0,
+        user: 0
+    }
 
-    // Ask computer to choose rock, paper, or scissors and store in variable compAns
+    isPlaying = True;
+    // while user wants to play
+    while (isPlaying) {
+        // Ask computer to choose rock, paper, or scissors and store in variable compAns
+        compAns = getCompAns();
 
-    // Ask user to choose rock, paper, or scissors and store in variable userAns
+        // Ask user to choose rock, paper, or scissors and store in variable userAns
+        userAns = getUserAns();
 
-    // Show winner 
+        // Find winner and store in variable winner
+        winner = getWinner(compAns, userAns);
 
-    // Show score
+        // Update score
+        updateScore(winner);
 
-    // Ask user if they want to continue playing
+        // Show score
+        console.log(`Score: Computer ${scores.computer} - User ${scores.user}`)
 
+        // Ask user if they want to continue playing and store in variable 
+    }
 }
